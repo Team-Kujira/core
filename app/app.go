b5c963b9fb10ff8d7f0264198a4373bb723ecfc4
codehans <94654388+codehans@users.noreply.github.com>
2023-02-27 18:28:42 +0000

    RunMigrations

--- app/app.go
+++ app/app.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -144,10 +143,11 @@ const (
 	AccountAddressPrefix = "kujira"
 	Name                 = "kujira"
 	// Either the SDK or some combination of the creation of the alliance module
-	// in 0.8.0 means that deleting the module leaves remenants at the original store key.
+	// in 0.8.0 means that deleting the module leaves remenants at the original store key,
+	// preventing a re-add at the same key.
 	// This puts the module storage under a new key, meaning we can bypass the faulty removal
 	// of the old module store
-	// N.B don't use the original
+	// N.B don't use the original!
 	AllianceStoreKey = "alliance2"
 )
 
@@ -461,6 +461,8 @@ func New(
 			); err != nil {
 				return nil, err
 			}
+			return app.mm.RunMigrations(ctx, cfg, fromVM)
+		})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
