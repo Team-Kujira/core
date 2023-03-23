5dcecc4a79c6fcbf8bfa1d3c5fe84470d11c2f59
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-23 16:34:00 +0700

    update params folder

--- cmd/kujirad/main.go
+++ cmd/kujirad/main.go
@@ -4,22 +4,21 @@ import (
 	"os"
 
 	"github.com/Team-Kujira/core/app"
-
+	"github.com/Team-Kujira/core/cmd/migalood/cmd"
+	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/ignite/cli/ignite/pkg/cosmoscmd"
 )
 
 func main() {
-	rootCmd, _ := cosmoscmd.NewRootCmd(
-		app.Name,
-		app.AccountAddressPrefix,
-		app.DefaultNodeHome,
-		app.Name,
-		app.ModuleBasics,
-		app.NewIgniteApp,
-		// this line is used by starport scaffolding # root/arguments
-	)
-	if err := svrcmd.Execute(rootCmd, "KUJIRAD", app.DefaultNodeHome); err != nil {
-		os.Exit(1)
+	rootCmd, _ := cmd.NewRootCmd()
+
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
 	}
 }
