21ffaaadd168e4dd45b526fee52ce10bda44211e
Jacob Gadikian <jacobgadikian@gmail.com>
2023-06-03 17:58:55 +0800

    linting related changes

--- x/oracle/module.go
+++ x/oracle/module.go
@@ -81,7 +81,7 @@ func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 	return cli.GetQueryCmd()
 }
 
-//___________________________
+// ___________________________
 
 // AppModule implements an application module for the oracle module.
 type AppModule struct {
@@ -161,7 +161,7 @@ func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.Val
 	return []abci.ValidatorUpdate{}
 }
 
-//____________________________________________________________________________
+// ____________________________________________________________________________
 
 // AppModuleSimulation functions
 
