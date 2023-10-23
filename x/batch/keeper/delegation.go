6866094ddc354bd2a5dfac072c83eaeb6f328b7d
antstalepresh <stalepresh121@outlook.com>
2023-10-23 09:29:48 -0400

    code cleaning, bug fixing for reward distribution issue

--- x/batch/keeper/delegation.go
+++ x/batch/keeper/delegation.go
@@ -55,10 +55,17 @@ func (k Keeper) withdrawAllDelegationRewards(ctx sdk.Context, delAddr sdk.AccAdd
 	k.stakingKeeper.IterateDelegations(ctx, delAddr, func(_ int64, del stakingtypes.DelegationI) (stop bool) {
 		valAddr := del.GetValidatorAddr()
 		val := k.stakingKeeper.Validator(ctx, valAddr)
+
+		// check existence of delegator starting info
+		if !k.distrKeeper.HasDelegatorStartingInfo(ctx, del.GetValidatorAddr(), del.GetDelegatorAddr()) {
+			return false
+		}
+
 		// end current period and calculate rewards
 		endingPeriod := k.distrKeeper.IncrementValidatorPeriod(ctx, val)
 		rewardsRaw := k.distrKeeper.CalculateDelegationRewards(ctx, val, del, endingPeriod)
 		outstanding := k.distrKeeper.GetValidatorOutstandingRewardsCoins(ctx, del.GetValidatorAddr())
+
 		// defensive edge case may happen on the very final digits
 		// of the decCoins due to operation order of the distribution mechanism.
 		rewards := rewardsRaw.Intersect(outstanding)
@@ -72,16 +79,24 @@ func (k Keeper) withdrawAllDelegationRewards(ctx sdk.Context, delAddr sdk.AccAdd
 				"expected", rewardsRaw.String(),
 			)
 		}
-		k.distrKeeper.SetValidatorOutstandingRewards(ctx, del.GetValidatorAddr(), types.ValidatorOutstandingRewards{Rewards: outstanding.Sub(rewards)})
+
 		// truncate reward dec coins, return remainder to community pool
 		finalRewards, remainder := rewards.TruncateDecimal()
-		remainderTotal.Add(remainder...)
+		remainderTotal = remainderTotal.Add(remainder...)
+		rewardsTotal = rewardsTotal.Add(finalRewards...)
+
+		// update the outstanding rewards and the community pool only if the
+		// transaction was successful
+		k.distrKeeper.SetValidatorOutstandingRewards(ctx, del.GetValidatorAddr(), types.ValidatorOutstandingRewards{Rewards: outstanding.Sub(rewards)})
+
 		// decrement reference count of starting period
 		startingInfo := k.distrKeeper.GetDelegatorStartingInfo(ctx, del.GetValidatorAddr(), del.GetDelegatorAddr())
 		startingPeriod := startingInfo.PreviousPeriod
 		k.decrementReferenceCount(ctx, del.GetValidatorAddr(), startingPeriod)
+
+		// reinitialize the delegation
 		k.initializeDelegation(ctx, valAddr, delAddr)
-		rewardsTotal = rewardsTotal.Add(finalRewards...)
+
 		ctx.EventManager().EmitEvent(
 			sdk.NewEvent(
 				types.EventTypeWithdrawRewards,
@@ -91,11 +106,13 @@ func (k Keeper) withdrawAllDelegationRewards(ctx sdk.Context, delAddr sdk.AccAdd
 		)
 		return false
 	})
-	// distribute to community pool
+
+	// distribute total remainder to community pool
 	feePool := k.distrKeeper.GetFeePool(ctx)
 	feePool.CommunityPool = feePool.CommunityPool.Add(remainderTotal...)
 	k.distrKeeper.SetFeePool(ctx, feePool)
-	// add coins to user account
+
+	// add total reward coins to user account
 	if !rewardsTotal.IsZero() {
 		withdrawAddr := k.distrKeeper.GetDelegatorWithdrawAddr(ctx, delAddr)
 		err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, withdrawAddr, rewardsTotal)
