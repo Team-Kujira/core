d1f683073cd39c728f2e2cc97b7bc4d40ea8faca
codehans <94654388+codehans@users.noreply.github.com>
2023-10-25 11:14:58 +0100

    proto

--- scripts/protocgen-wrapper.sh
+++ scripts/protocgen-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if which docker > /dev/null; then
-    docker run --volume "$(pwd)\:/workspace" --workdir /workspace ghcr.io/cosmos/proto-builder:0.12.1 sh ./scripts/protocgen.sh
-elif which podman > /dev/null; then
-    podman run --rm -v $(pwd):/workspace --workdir /workspace -u root ghcr.io/cosmos/proto-builder:0.12.1 sh ./scripts/protocgen.sh
+    docker run --volume "$(pwd):/workspace" --workdir /workspace ghcr.io/cosmos/proto-builder:0.14.0 sh ./scripts/protocgen.sh
+    elif which podman > /dev/null; then
+    podman run --rm -v $(pwd):/workspace --workdir /workspace -u root ghcr.io/cosmos/proto-builder:0.14.0 sh ./scripts/protocgen.sh
 fi
