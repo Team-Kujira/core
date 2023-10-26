a342d0f7fb7faa066224aab015e0c682e6a23fb4
antstalepresh <stalepresh121@outlook.com>
2023-10-25 22:41:23 -0400

    add gas usage result to unit tests

--- x/batch/keeper/keeper_test.go
+++ x/batch/keeper/keeper_test.go
@@ -8,24 +8,6 @@ import (
 	tmtypes "github.com/cometbft/cometbft/proto/tendermint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
-
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
-	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
-)
-
-var (
-	PKS = simtestutil.CreateTestPubKeys(5)
-
-	valConsPk0 = PKS[0]
-	valConsPk1 = PKS[1]
-	valConsPk2 = PKS[2]
-
-	valConsAddr0 = sdk.ConsAddress(valConsPk0.Address())
-	valConsAddr1 = sdk.ConsAddress(valConsPk1.Address())
-	valConsAddr2 = sdk.ConsAddress(valConsPk2.Address())
-
-	valConsAddrs = []sdk.ConsAddress{valConsAddr0, valConsAddr1, valConsAddr2}
-	valConsPks   = []cryptotypes.PubKey{valConsPk0, valConsPk1, valConsPk2}
 )
 
 type KeeperTestSuite struct {
