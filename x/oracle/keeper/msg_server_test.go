5a9194a567060f556379ecefbaede1dffd312aec
antstalepresh <stalepresh121@outlook.com>
2024-07-12 21:49:45 +0800

    unit test for UpdateParams, AddRequiredDenoms, RemoveRequiredDenoms

--- x/oracle/keeper/msg_server_test.go
+++ x/oracle/keeper/msg_server_test.go
@@ -3,7 +3,6 @@ package keeper
 import (
 	"testing"
 
-	"cosmossdk.io/math"
 	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -14,8 +13,7 @@ import (
 )
 
 var (
-	stakingAmt         = sdk.TokensFromConsensusPower(10, sdk.DefaultPowerReduction)
-	randomExchangeRate = math.LegacyNewDec(1700)
+	stakingAmt = sdk.TokensFromConsensusPower(10, sdk.DefaultPowerReduction)
 )
 
 func setup(t *testing.T) (TestInput, types.MsgServer) {
@@ -39,4 +37,119 @@ func setup(t *testing.T) (TestInput, types.MsgServer) {
 	input.StakingKeeper.EndBlocker(input.Ctx)
 
 	return input, msgServer
+}
+
+func TestMsgUpdateParams(t *testing.T) {
+	input, msgServer := setup(t)
+
+	// Test default params setting
+	input.OracleKeeper.SetParams(input.Ctx, types.DefaultParams())
+	params := input.OracleKeeper.GetParams(input.Ctx)
+	require.NotNil(t, params)
+
+	// Test updating with invalid authority
+	_, err := msgServer.UpdateParams(input.Ctx, &types.MsgUpdateParams{
+		Authority: "invalid_authority",
+		Params:    &params,
+	})
+	require.Error(t, err)
+
+	// Test updating with correct authority
+	_, err = msgServer.UpdateParams(input.Ctx, &types.MsgUpdateParams{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Params:    &params,
+	})
+	require.NoError(t, err)
+
+	// Test updating required denoms
+	params.RequiredDenoms = []types.Denom{{Denom: "BTC", Id: 1}}
+	_, err = msgServer.UpdateParams(input.Ctx, &types.MsgUpdateParams{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Params:    &params,
+	})
+	require.Error(t, err)
+}
+
+func TestMsgAddRequiredDenoms(t *testing.T) {
+	input, msgServer := setup(t)
+
+	params := input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 3)
+	require.Equal(t, params.LastDenomId, uint32(3))
+
+	// Test with invalid authority
+	_, err := msgServer.AddRequiredDenoms(input.Ctx, &types.MsgAddRequiredDenoms{
+		Authority: "invalid_authority",
+		Symbols:   []string{"ATOM"},
+	})
+	require.Error(t, err)
+
+	// Adding a single denom
+	_, err = msgServer.AddRequiredDenoms(input.Ctx, &types.MsgAddRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"ATOM"},
+	})
+	require.NoError(t, err)
+	params = input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 4)
+	require.Equal(t, params.LastDenomId, uint32(4))
+
+	// Adding two denoms
+	_, err = msgServer.AddRequiredDenoms(input.Ctx, &types.MsgAddRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"AKT", "ARB"},
+	})
+	require.NoError(t, err)
+	params = input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 6)
+	require.Equal(t, params.LastDenomId, uint32(6))
+
+	// Adding already existing denom
+	_, err = msgServer.AddRequiredDenoms(input.Ctx, &types.MsgAddRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"ATOM"},
+	})
+	require.Error(t, err)
+}
+
+func TestMsgRemoveRequiredDenoms(t *testing.T) {
+	input, msgServer := setup(t)
+
+	params := input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 3)
+	require.Equal(t, params.LastDenomId, uint32(3))
+
+	// Test with invalid authority
+	_, err := msgServer.RemoveRequiredDenoms(input.Ctx, &types.MsgRemoveRequiredDenoms{
+		Authority: "invalid_authority",
+		Symbols:   []string{"BTC"},
+	})
+	require.Error(t, err)
+
+	// Removing a single denom
+	_, err = msgServer.RemoveRequiredDenoms(input.Ctx, &types.MsgRemoveRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"BTC"},
+	})
+	require.NoError(t, err)
+	params = input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 2)
+	require.Equal(t, params.LastDenomId, uint32(3))
+
+	// Removing two denoms
+	_, err = msgServer.RemoveRequiredDenoms(input.Ctx, &types.MsgRemoveRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"ETH", "USDT"},
+	})
+	require.NoError(t, err)
+	params = input.OracleKeeper.GetParams(input.Ctx)
+	require.Len(t, params.RequiredDenoms, 0)
+	require.Equal(t, params.LastDenomId, uint32(3))
+
+	// Removing not existing denom
+	_, err = msgServer.RemoveRequiredDenoms(input.Ctx, &types.MsgRemoveRequiredDenoms{
+		Authority: "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		Symbols:   []string{"BTC"},
+	})
+	require.NoError(t, err)
 }
